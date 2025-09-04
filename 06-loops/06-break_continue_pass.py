fae2db470755a9ccf843e2d2b1945327cdf9879b
Sergio Barreras <sergiobg.isc@gmail.com>
2025-09-04 17:32:21 -0600

    Add example code for pass, break, and continue statements in loops

---
+++ 06-loops/06-break_continue_pass.py
@@ -0,0 +1,17 @@
+# Pass
+# for i in range(5):
+#     pass
+
+# Break
+# for i in range(5):
+#     if i == 3:
+#         break
+#     print(i)
+
+# Continue
+number = 0
+while number in range(10):
+    number += 1
+    continue
+
+print(number)
