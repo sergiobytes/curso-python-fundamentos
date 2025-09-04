9ecac6fcbdbe84067d1e01e5ec203e7912c14ef2
Sergio Barreras <sergiobg.isc@gmail.com>
2025-09-04 17:14:30 -0600

    Refactor while loop example to focus on user input and exit condition

--- 06-loops/05-while.py
+++ 06-loops/05-while.py
@@ -1,5 +1,14 @@
 counter = 1
 
-while counter <= 5:
-    print(f'Number: {counter}')
-    counter += 1
+# while counter <= 5:
+#     print(f'Number: {counter}')
+#     counter += 1
+# else:
+#     print('Loop is done!')
+
+response = ''
+
+while response.lower() != 'quit':
+    response = input('Type "quit" to exit: ')
+else:
+    print('Loop is done!')
