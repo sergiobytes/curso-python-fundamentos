ef21819779f7c25dc16b60db9e34c8609dade06f
Sergio Barreras <sergiobg.isc@gmail.com>
2025-09-16 08:53:02 -0600

    Add hello and bye functions with print statements and return values

---
+++ 07-funciones/02-return.py
@@ -0,0 +1,14 @@
+def hello():
+    print("Hola Mundo desde función")
+
+    return 'Hola desde return'
+
+
+def bye():
+    print("Adiós Mundo desde función")
+
+    return 'Adiós desde return'
+
+
+print(hello())
+print(bye())
